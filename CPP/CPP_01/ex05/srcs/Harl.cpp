0ce60fc4540d14cca6515c0921740bf7dedc8733
GrzegorzBuczynski <grzegorzbuczynski93@gmail.com>
2025-01-03 20:57:52 +0100

    make push!

--- CPP/CPP_01/ex05/srcs/Harl.cpp
+++ CPP/CPP_01/ex05/srcs/Harl.cpp
@@ -6,9 +6,51 @@
 /*   By: gbuczyns <gbuczyns@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:37:00 by gbuczyns          #+#    #+#             */
-/*   Updated: 2025/01/03 17:55:15 by gbuczyns         ###   ########.fr       */
+/*   Updated: 2025/01/03 20:57:05 by gbuczyns         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Harl.hpp"
 # include <iostream>
+
+void Harl::debug(void)
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple";
+    std::cout << "-pickle-specialketchup burger. I really do!" << std::endl;    
+}
+
+void Harl::info(void)
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. ";
+    std::cout << "You didn’t put enough bacon in my burger! If you did, ";
+    std::cout << "I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void)
+{
+    std::cout << "I think I deserve to have some extra bacon for free. ";
+    std::cout << "I’ve been coming for years whereas you started working ";
+    std::cout << "here since last month." << std::endl;
+}
+
+void Harl::error(void)
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now.";
+    std::cout << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+    void (Harl::*complaints[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    for (int i = 0; i < 4; i++)
+    {
+        if (level == levels[i])
+        {
+            (this->*complaints[i])();
+            return;
+        }
+    }
+    std::cout << "Unknown level: " << level << std::endl;
+}
--- CPP/CPP_01/ex05/srcs/main.cpp
+++ CPP/CPP_01/ex05/srcs/main.cpp
@@ -6,7 +6,33 @@
 /*   By: gbuczyns <gbuczyns@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:36:57 by gbuczyns          #+#    #+#             */
-/*   Updated: 2025/01/03 17:36:58 by gbuczyns         ###   ########.fr       */
+/*   Updated: 2025/01/03 20:57:12 by gbuczyns         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Harl harl;
+	std::string level;
+
+	while (true)
+	{
+		std::cout << "Enter complaint level (DEBUG, INFO, WARNING,";
+		std::cout << " ERROR) or 'end' to quit: ";
+		if (!std::getline(std::cin, level))
+		{
+			std::cout << std::endl;
+			break ;
+		}
+		if (level == "end")
+		{
+			break ;
+		}
+
+		harl.complain(level);
+	}
+	return (0);
+}
