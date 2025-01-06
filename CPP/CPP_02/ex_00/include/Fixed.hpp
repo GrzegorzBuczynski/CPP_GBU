5c4e2d872c6366a81cc902890c8c83e42ba45ddd
Surface <grzegorzbuczynski93@gmail.com>
2025-01-06 22:47:34 +0100

    make push!

---
+++ CPP/CPP_02/ex_00/include/Fixed.hpp
@@ -0,0 +1,24 @@
+
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+    private:
+        int _fixedPointValue;
+        static const int fractionalBits = 8;
+    public:
+        Fixed();
+        // Fixed(); // Default constructor
+        Fixed(const Fixed &fixed); // Copy constructor
+        Fixed &operator=(const Fixed &other); // Copy assignment operator
+        ~Fixed(); // Destructor
+
+        int getRawBits(void) const;
+        void setRawBits(int const raw);        
+};
+
+#endif
---
+++ CPP/CPP_02/ex_00/srcs/Fixed.cpp
@@ -0,0 +1,44 @@
+
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fixedPointValue(0) 
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    // Output a message indicating the copy assignment operator is called
+    std::cout << "Copy assignment operator called" << std::endl;
+    
+    // Check for self-assignment
+    if (this != &other)
+        // Copy the value from the other object
+        this->_fixedPointValue = other.getRawBits();
+    // Return the current object
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_fixedPointValue = raw;
+}
