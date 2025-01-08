c29276f4c670372ff38dc41e8a35c221466da526
Surface <grzegorzbuczynski93@gmail.com>
2025-01-08 21:57:58 +0100

    make push!

---
+++ CPP/CPP_02/ex01/srcs/Fixed.cpp
@@ -0,0 +1,74 @@
+
+
+#include "Fixed.hpp"
+# include <iostream>
+
+
+Fixed::Fixed() : _fixedPointValue(0) 
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value)
+{
+    std::cout << "Int constructor called" << std::endl;
+    _fixedPointValue = value << _fractionalBits;
+}
+
+Fixed::Fixed(const float value)
+{
+    std::cout << "Float constructor called" << std::endl;
+    _fixedPointValue = roundf(value * (1 << _fractionalBits));
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    // Output a message indicating the copy assignment operator is called
+    std::cout << "Copy assignment operator called" << std::endl;
+    
+    // Check for self-assignment
+    if (this != &other)
+        // Copy the value from the other object
+        this->_fixedPointValue = other.getRawBits();
+    // Return the current object
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_fixedPointValue = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+    return static_cast<float>(_fixedPointValue) / (1 << _fractionalBits);
+}
+
+int Fixed::toInt(void) const
+{
+    return _fixedPointValue >> _fractionalBits;
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+    out << fixed.toFloat();
+    return out;
+}
--- CPP/CPP_02/ex_00/include/Fixed.hpp
+++ CPP/CPP_02/ex_00/include/Fixed.hpp
@@ -3,14 +3,14 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+
 class Fixed
 {
     private:
         int _fixedPointValue;
-        static const int fractionalBits = 8;
+        static const int _fractionalBits = 8;
     public:
         Fixed();
-        // Fixed(); // Default constructor
         Fixed(const Fixed &fixed); // Copy constructor
         Fixed &operator=(const Fixed &other); // Copy assignment operator
         ~Fixed(); // Destructor
