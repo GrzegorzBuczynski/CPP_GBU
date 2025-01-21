fa95b45a5ecda28e993ad1c43f58e5f0dccd21ae
Surface <grzegorzbuczynski93@gmail.com>
2025-01-21 06:20:19 +0100

    make push!

--- CPP/CPP_04/ex00/srcs/main.cpp
+++ CPP/CPP_04/ex00/srcs/main.cpp
@@ -1,16 +1,42 @@
-#include "ClapTrap.hpp"
+# include "Animal.hpp"
+# include "Dog.hpp"
+# include "Cat.hpp"
+# include "WrongAnimal.hpp"
+# include "WrongCat.hpp"
 
-int main()
+
+int main() 
 {
-    ClapTrap claptrap("ABC");
+    // Create instances of Animal, Dog, and Cat
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
 
-    claptrap.attack("Target1");
-    claptrap.takeDamage(10);
-    claptrap.beRepaired(5);
+    // Display the types of Dog and Cat
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
 
-    claptrap.attack("Target2");
-    claptrap.takeDamage(100);
-    claptrap.beRepaired(50);
+    // Call makeSound() methods
+    j->makeSound(); // will output the dog sound!
+    i->makeSound(); // will output the cat sound!
+    meta->makeSound(); // will output the animal sound!
+
+    // Clean up
+    delete meta;
+    delete j;
+    delete i;
+
+    // Create instances of WrongAnimal and WrongCat
+    const WrongAnimal* wrongMeta = new WrongAnimal();
+    const WrongAnimal* wrongI = new WrongCat();
+
+    // Call makeSound() methods
+    std::cout << wrongI->getType() << " " << std::endl;
+    wrongI->makeSound(); // will output the WrongAnimal sound!
+
+    // Clean up
+    delete wrongMeta;
+    delete wrongI;
 
     return 0;
 }
